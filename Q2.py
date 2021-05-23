e3f3fb01d760943339bcaf04a5ea8f60cf9677e8
amit <amitsr4@gmail.com>
2021-05-23 16:58:06 +0300

    Q2 done

--- Q2.py
+++ Q2.py
@@ -13,7 +13,7 @@ f[(2*one):3*one] = 0.7 - 0.2*x[(2*one):3*one];
 f[(3*one):4*one] = 0.3
 f[(4*one):(5*one)] = 0.5 - 0.1*x[(4*one):(5*one)];
 
-G = spdiags([-np.ones(n), np.ones(n)], np.array([0,1]), n-1,n).toarray()
+G = spdiags([-np.ones(n), np.ones(n)], np.array([0,1]), n,n).toarray()
 etta = 0.1*np.random.randn(np.size(x));
 y = f + etta
 # plt.figure(); plt.plot(x,y, label = "noisy"); plt.plot(x,f, label = "clean"); plt.legend() ;plt.show()
@@ -27,11 +27,20 @@ GTG = np.transpose(G) @ G
 M = np.eye(n) + lam/2 * GTG
 x_min = LA.inv(M) @ y
 
-plt.figure(); plt.plot(x,x_min, label = "recover"); plt.plot(x,f, label = "clean"); plt.legend() ;plt.show()
+# plt.figure(); plt.plot(x,x_min, label = "recover"); plt.plot(x,f, label = "clean"); plt.legend() ;plt.show()
 
 
 # --------------------------------------------(b)-------------------------------------------------
 
-def IRLS(x, y, w):
+def IRLS( y, w, G ,  epsilon, lamda, maxIter):
 
+    for i in range(maxIter):
+        x = LA.inv(np.eye(n) + lamda/2 * (np.transpose(G) @ w @ G)) @ y
+        for j in range (len(w)):
+            w[j][j] = 1/ (abs(G[:,j] @ x)+ epsilon)
 
+    return x
+
+x_min = IRLS( y,  np.eye(n), G, 0.001, 1, 10)
+
+plt.figure(); plt.plot(x,x_min, label = "recover"); plt.plot(x,f, label = "clean"); plt.legend() ;plt.show()
