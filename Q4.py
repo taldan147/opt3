41a0d0689c4821120856411e6369b8d1153a5530
taldan147 <tald96@gmailc.om>
2021-06-03 17:00:17 +0300

    3/6

--- Q4.py
+++ Q4.py
@@ -1,9 +1,15 @@
+import copy
+
 import numpy as np
 from scipy.sparse import spdiags
 import matplotlib.pyplot as plt
 import numpy.linalg as LA
 import math
-
+import struct
+from array import array
+from os.path import join
+import os
+import random
 
 def LGobj(X, y ,w):
     c1, c2 = generate_c(y)
@@ -25,7 +31,10 @@ def comp_sigmoid(v):
 def vec_log(v):
     log_vec =[]
     for i in range(len(v)):
-        log_vec.append(math.log(v[i]))
+        if v[i] > 0:
+            log_vec.append(math.log(v[i]))
+        else:
+            log_vec.append(v[i])
     return np.asarray(log_vec)
 
 def gradientLogistic(X,y,w):
@@ -117,36 +126,259 @@ def test_hess(X,y,w , epsilon, d, maxIter):
 
     return loss, loss_with_hess, epsilons, DF, DF_hess
 
-# def test_Jacobian(X,y,w , epsilon, d, maxIter):
 
 
 loss , loss_grad, epsilons, DF, DF_grad = test_grad(X, y, w, 0.1, d, 20)
 iterations = np.arange(19)
 print(DF_grad)
-plt.figure(); plt.plot(iterations,DF, label = "loss");  plt.plot(iterations,DF_grad, label = "loss_grad"); plt.legend() ;plt.show()
+# plt.figure(); plt.semilogy(iterations,loss, label = "gradient test : loss");  plt.semilogy(iterations,loss_grad, label = "loss_grad");
+plt.figure(); plt.plot(iterations,DF, label = "gradient test : loss");  plt.plot(iterations,DF_grad, label = "loss_grad");
+
 
 loss , loss_hess, epsilons, DF, DF_hess = test_hess(X, y, w, 0.1, d, 20)
-plt.figure(); plt.plot(iterations,DF, label = "loss");  plt.plot(iterations,DF_hess, label = "loss_hess"); plt.legend() ;plt.show()
+# plt.semilogy(iterations,loss, label = "hessian test : loss");  plt.semilogy(iterations,loss_hess, label = "loss_hess"); plt.legend() ;plt.show()
+plt.plot(iterations,DF, label = "hessian test : loss");  plt.plot(iterations,DF_hess, label = "loss_hess"); plt.legend() ;plt.show()
+
 
 # --------------------------------------------------------c-----------------------------------------------------------
+#
+# MNIST Data Loader Class
+#
+class MnistDataloader(object):
+    def __init__(self, training_images_filepath, training_labels_filepath,
+                 test_images_filepath, test_labels_filepath):
+        self.training_images_filepath = training_images_filepath
+        self.training_labels_filepath = training_labels_filepath
+        self.test_images_filepath = test_images_filepath
+        self.test_labels_filepath = test_labels_filepath
 
-def Armijio(x,  gradx, d, a, b, c, maxiter):
+    def read_images_labels(self, images_filepath, labels_filepath):
+        labels = []
+        with open(labels_filepath, 'rb') as file:
+            magic, size = struct.unpack(">II", file.read(8))
+            if magic != 2049:
+                raise ValueError('Magic number mismatch, expected 2049, got {}'.format(magic))
+            labels = array("B", file.read())
+
+        with open(images_filepath, 'rb') as file:
+            magic, size, rows, cols = struct.unpack(">IIII", file.read(16))
+            if magic != 2051:
+                raise ValueError('Magic number mismatch, expected 2051, got {}'.format(magic))
+            image_data = array("B", file.read())
+        images = []
+        for i in range(size):
+            images.append([0] * rows * cols)
+        for i in range(size):
+            img = np.array(image_data[i * rows * cols:(i + 1) * rows * cols])
+            img = img.reshape(28, 28)
+            meanval = np.mean(img)
+            stdval = np.std(img)
+            img = (img - meanval) / (stdval + 0.1)
+            images[i][:] = img
+
+        return images, labels
+
+    def load_data(self):
+        x_train, y_train = self.read_images_labels(self.training_images_filepath, self.training_labels_filepath)
+        x_test, y_test = self.read_images_labels(self.test_images_filepath, self.test_labels_filepath)
+        return (x_train, y_train), (x_test, y_test)
+
+    #
+
+
+# Verify Reading Dataset via MnistDataloader class
+#
+#
+# Set file paths based on added MNIST Datasets
+#
+cwd = os.getcwd()
+input_path = cwd + '\MNIST'
+training_images_filepath = join(input_path, 'train-images-idx3-ubyte\\train-images.idx3-ubyte')
+training_labels_filepath = join(input_path, 'train-labels-idx1-ubyte\\train-labels.idx1-ubyte')
+test_images_filepath = join(input_path, 't10k-images-idx3-ubyte\\t10k-images.idx3-ubyte')
+test_labels_filepath = join(input_path, 't10k-labels-idx1-ubyte\\t10k-labels.idx1-ubyte')
+
+
+#
+# Helper function to show a list of images with their relating titles
+#
+def show_images(images, title_texts):
+    cols = 5
+    rows = int(len(images) / cols) + 1
+    plt.figure(figsize=(30, 20))
+    index = 1
+    for x in zip(images, title_texts):
+        image = x[0]
+        title_text = x[1]
+        plt.subplot(rows, cols, index)
+        plt.imshow(image, cmap=plt.cm.gray)
+        if (title_text != ''):
+            plt.title(title_text, fontsize=15);
+        index += 1
+    plt.show()
+
+
+#
+# Load MINST dataset
+#
+mnist_dataloader = MnistDataloader(training_images_filepath, training_labels_filepath, test_images_filepath,
+                                   test_labels_filepath)
+(x_train, y_train), (x_test, y_test) = mnist_dataloader.load_data()
+
+#
+# Show some random training and test images
+#
+images_2_show = []
+titles_2_show = []
+for i in range(0, 10):
+    r = random.randint(1, 60000)
+    images_2_show.append(x_train[r])
+    titles_2_show.append('training image [' + str(r) + '] = ' + str(y_train[r]))
+
+for i in range(0, 5):
+    r = random.randint(1, 10000)
+    images_2_show.append(x_test[r])
+    titles_2_show.append('test image [' + str(r) + '] = ' + str(y_test[r]))
+
+show_images(images_2_show, titles_2_show)
+
+
+def Armijio(X,y,x, gradx, d, a, b, c, maxiter):
     for i in range(maxiter):
-        obj_a = LG(X, y, x + a*d)
-        if obj_a < LG(x) + c*a*(gradx @ d):
+        obj_a = LGobj(X, y, x + a*d)
+        if obj_a < (LGobj(X, y, x) + c*a*np.inner(gradx,d)):
             return a
         else:
             a = b*a
     return a
 
+
 def gradient_descent(X, y, w,maxIter, a0, beta, c, epsilon):
-    f_values = [LG(X, y, w)]
+    f_values = [LGobj(X, y, w)]
     for i in range(maxIter):
         grad = gradientLogistic(X,y,w)
         d = -grad
-        alpha = Armijio(w, grad, d, a0,beta, c, 100)
+        alpha = Armijio(X,y,w, grad, d, a0,beta, c, 100)
         w = w + alpha * d
-        f_values.append(LG(X,y,w))
+        # w = np.clip(w,-1,1)
+        f_values.append(LGobj(X,y,w))
         if LA.norm(w-f_values[i-1]) / LA.norm(w) < epsilon:
             break
-        return w,
+    return w, f_values
+
+def exactNewton(X, y, w,maxIter, a0, beta, c, epsilon):
+    f_values = [LGobj(X, y, w)]
+    for i in range(maxIter):
+        grad = gradientLogistic(X,y,w)
+        hess = hessianLogistic(X,w) + np.eye(len(w))* 0.001
+        d = -1 * LA.inv(hess) @ grad
+        alpha = Armijio(X,y,w, grad, d, a0,beta, c, 100)
+        w = w + alpha * d
+        # w = np.clip(w,-1,1)
+        f_values.append(LGobj(X,y,w))
+        if LA.norm(w-f_values[i-1]) / LA.norm(w) < epsilon:
+            break
+    return w, f_values
+
+
+def filterTwoDigits(num1, num2, x, y):
+    x_res=[]
+    y_res = []
+    for i in range(len(x)):
+        if y[i] == num1 or y[i] == num2:
+            x_res.append(x[i])
+            y_res.append(y[i])
+    return x_res ,y_res
+
+def imageToVector(data):
+    xNew = copy.deepcopy(data)
+    for i in range(len(xNew)):
+        tmpMatrix = xNew[i]
+        tmp = np.ndarray.tolist(tmpMatrix[0])
+        for j in range(1, len(tmpMatrix)):
+            list = np.ndarray.tolist(tmpMatrix[j])
+            tmp.extend(list)
+        xNew[i] = np.asarray(tmp)
+    return xNew
+
+
+def calculateError(w, w_k):
+    ans = []
+    for i in range(len(w_k)):
+        ans.append(abs(w_k[i]-w))
+    return ans;
+
+
+
+
+
+def runAndPlotGD(digit1, digit2):
+    zero_one_images_train, zero_one_labels_train = filterTwoDigits(digit1, digit2, x_train, y_train)
+    zero_one_images_train = imageToVector(zero_one_images_train)
+    zero_one_images_test, zero_one_labels_test = filterTwoDigits(digit1, digit2, x_test, y_test)
+    zero_one_images_test = imageToVector(zero_one_images_test)
+
+    # for i in range(len(zero_one_images_train)):
+    #     zero_one_images_train[i] = (zero_one_images_train[i]) / 255
+    #
+    # for i in range(len(zero_one_images_test)):
+    #     zero_one_images_test[i] = (zero_one_images_test[i]) / 255
+    zero_one_images_train = np.transpose(np.asarray(zero_one_images_train))
+    zero_one_labels_train = np.asarray(zero_one_labels_train)
+    zero_one_images_test = np.transpose(np.asarray(zero_one_images_test))
+    zero_one_labels_test = np.asarray(zero_one_labels_test)
+
+    w1 = np.zeros(784)
+    bet = 0.5
+    c = 1e-4
+    # def gradient_descent(X, y, w,maxIter, a0, beta, c, epsilon):
+    wStar, w_k = gradient_descent(zero_one_images_train, zero_one_labels_train, w1, 100, 0.25, bet, c, 0.0001)
+    error_train = calculateError(w_k[len(w_k) - 1], w_k)
+    wStar_test, w_k_test = gradient_descent(zero_one_images_test, zero_one_labels_test, w1, 100, 0.25, bet, c, 0.0001)
+    # error_test = calculateError(min(w_k_test), w_k_test)
+    error_test = calculateError(w_k_test[len(w_k) - 1], w_k_test)
+
+    plt.semilogy(error_train, label='train '+str(digit1)+',' + str(digit2))
+    plt.semilogy(error_test, label='test '+str(digit1)+',' + str(digit2))
+    plt.xlabel("iteration")
+    plt.ylabel(r'$|f(w^{(k)}-f(w^*)|$')
+    plt.legend()
+    plt.show()
+
+def runAndPlotNewton(digit1, digit2):
+    zero_one_images_train, zero_one_labels_train = filterTwoDigits(digit1, digit2, x_train, y_train)
+    zero_one_images_train = imageToVector(zero_one_images_train)
+    zero_one_images_test, zero_one_labels_test = filterTwoDigits(digit1, digit2, x_test, y_test)
+    zero_one_images_test = imageToVector(zero_one_images_test)
+
+    for i in range(len(zero_one_images_train)):
+        zero_one_images_train[i] = (zero_one_images_train[i]) / 255
+
+    for i in range(len(zero_one_images_test)):
+        zero_one_images_test[i] = (zero_one_images_test[i]) / 255
+    zero_one_images_train = np.transpose(np.asarray(zero_one_images_train))
+    zero_one_labels_train = np.asarray(zero_one_labels_train)
+    zero_one_images_test = np.transpose(np.asarray(zero_one_images_test))
+    zero_one_labels_test = np.asarray(zero_one_labels_test)
+
+    w1 = np.zeros(784)
+    bet = 0.5
+    c = 1e-4
+    wStar, w_k = exactNewton(zero_one_images_train, zero_one_labels_train, w1, 100, 1, bet, c, 0.0001)
+    error_train = calculateError(w_k[len(w_k) - 1], w_k)
+    wStar_test, w_k_test = exactNewton(zero_one_images_test, zero_one_labels_test, w1, 100, 1, bet, c, 0.0001)
+    # error_test = calculateError(min(w_k_test), w_k_test)
+    error_test = calculateError(w_k_test[len(w_k) - 1], w_k_test)
+
+    plt.semilogy(error_train, label='train ' + str(digit1) + ',' + str(digit2))
+    plt.semilogy(error_test, label='test ' + str(digit1) + ',' + str(digit2))
+    plt.xlabel("iteration")
+    plt.ylabel(r'$|f(w^{(k)}-f(w^*)|$')
+    plt.legend()
+    plt.show()
+
+runAndPlotGD(0,1)
+# runAndPlotGD(8,9)
+
+# runAndPlotNewton(0,1)
+runAndPlotNewton(8,9)
